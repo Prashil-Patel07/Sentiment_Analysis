c304d00786a82cfbe5ba595df0df8afe0b4e3804
Prashil-Patel07 <prashilp07@gmail.com>
2025-04-30 18:35:10 +1200

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,16 @@ def run():
         st.session_state.show_image = not st.session_state.show_image
 
     if st.session_state.show_image:
-        st.image("https://www.atoti.io/wp-content/uploads/2024/06/1_t4zCD60p-dDd5BRmINW9tQ.webp", caption="Image from URL")
+        st.image("https://www.atoti.io/wp-content/uploads/2024/06/1_t4zCD60p-dDd5BRmINW9tQ.webp")
+
+    if "show_image" not in st.session_state:
+        st.session_state.show_image = False
+
+    if st.button("Click me to toggle image"):
+        st.session_state.show_image = not st.session_state.show_image
+
+    if st.session_state.show_image:
+        st.image("https://www.theinsaneapp.com/wp-content/uploads/2024/10/Data-Science-Meme-7.png")
 
     predicted_sentiment = ""
     if st.button("Predict"):
