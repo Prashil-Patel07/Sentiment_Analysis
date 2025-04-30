590450ccb69db66a2444e9f6d3c017d9d91f6e67
Prashil-Patel07 <prashilp07@gmail.com>
2025-04-30 18:40:58 +1200

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,6 @@ def run():
     st.text("")
     userinput = st.text_input('Enter text below, then click the Predict button.', placeholder='Input text HERE')
     st.text("")
-    if "show_image" not in st.session_state:
-        st.session_state.show_image = False
-
-    if st.button("Click me to toggle image"):
-        st.session_state.show_image = not st.session_state.show_image
-
-    if st.session_state.show_image:
-        st.image("https://www.atoti.io/wp-content/uploads/2024/06/1_t4zCD60p-dDd5BRmINW9tQ.webp", caption = 'Meme')
 
     if "show_image" not in st.session_state:
         st.session_state.show_image = False
@@ -28,6 +20,7 @@ def run():
 
     if st.session_state.show_image:
         st.image("https://www.theinsaneapp.com/wp-content/uploads/2024/10/Data-Science-Meme-7.png", caption = 'Meme')
+        st.image("https://www.atoti.io/wp-content/uploads/2024/06/1_t4zCD60p-dDd5BRmINW9tQ.webp", caption = 'Meme')
 
     predicted_sentiment = ""
     if st.button("Predict"):
