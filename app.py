bb05452e6d53e444dd960dad307d5d2f54b592eb
Prashil-Patel07 <prashilp07@gmail.com>
2025-05-01 15:07:11 +1200

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def run():
             st.session_state.show_image1 = not st.session_state.show_image1
     
         if st.session_state.show_image1:
-            st.image("https://www.theinsaneapp.com/wp-content/uploads/2024/10/Data-Science-Meme-7.png", caption="Image 1", use_column_width=True)
+            st.image("https://www.theinsaneapp.com/wp-content/uploads/2024/10/Data-Science-Meme-7.png", caption="Image 1")
     
     # Column 2: Button and Image 2
     with col2:
@@ -35,7 +35,7 @@ def run():
             st.session_state.show_image2 = not st.session_state.show_image2
     
         if st.session_state.show_image2:
-            st.image("https://www.atoti.io/wp-content/uploads/2024/06/1_t4zCD60p-dDd5BRmINW9tQ.webp", caption="Image 2", use_column_width=True)
+            st.image("https://www.atoti.io/wp-content/uploads/2024/06/1_t4zCD60p-dDd5BRmINW9tQ.webp", caption="Image 2")
 
     predicted_sentiment = ""
     if st.button("Predict"):
